d467ab3f3a31c050b26f30527bab12e795f52b5c
linh nguyen <thuynguy@student.hive.fi>
2024-03-02 21:47:35 +0200

    mariadb ready, wordpress wip

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+service mysql start;
+
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; 
+then
+    mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "FLUSH PRIVILEGES;"
+fi
+
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
