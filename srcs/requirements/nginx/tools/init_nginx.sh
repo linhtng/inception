30bc390fa1c918561cb85b81392b0e4b0bc25c52
linh nguyen <thuynguy@student.hive.fi>
2024-03-05 16:18:33 +0200

    docker compose seems working

---
+++ srcs/requirements/nginx/tools/init_nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f /etc/nginx/ssl/inception.crt ]; then
+    echo "Creating self-signed SSL certificate...";
+    openssl req -x509 -nodes -out /etc/nginx/ssl/inception.crt \
+	-keyout /etc/nginx/ssl/inception.key \
+	-subj "/CN=thuynguy.42.fr"
+    echo "Self-signed SSL certificate created.";
+fi
+
+exec "$@"
