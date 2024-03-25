6032fb7a32053aa341cb38e609fa8ad036c74e42
linhtng <linh.nguyen361991@gmail.com>
2024-03-25 19:56:20 +0200

    debian mariadb issue

--- Makefile
+++ Makefile
@@ -10,13 +10,14 @@ build: prepare
 	docker compose -f srcs/docker-compose.yml build
 
 # Start the services
-up:
+up: build
 	docker compose -f srcs/docker-compose.yml up -d
 
 # Stop the services
-down:
-	docker compose -f srcs/docker-compose.yml down -v
+clean:
+	docker rm -vf $$(docker ps -aq)
+	docker rmi -f $$(docker images -aq)
 
-fclean: down
+fclean: clean
 	rm -rf srcs/home/thuynguy/data/wordpress
 	rm -rf srcs/home/thuynguy/data/mariadb
--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -10,13 +10,16 @@ if [ -z "${MYSQL_USER}" ] || [ -z "${MYSQL_PASSWORD}" ] ; then
     exit 1
 fi
 
-mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
-chown -R mysql:mysql /var/lib/mysql /run/mysqld /var/log/mysql
+# mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
+# chown -R mysql:mysql /var/lib/mysql /run/mysqld /var/log/mysql
+# touch /var/log/mysql/error.log
 
 # Initialize MariaDB data directory and create system tables
+echo "Initializing MariaDB data directory"
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql >/dev/null
 
 # Start MariaDB service in the background
+echo "Starting MariaDB service"
 mysqld --user=mysql --skip-networking --socket=/run/mysqld/mysqld.sock --pid-file=/run/mysqld/mysqld.pid &
 
 # Wait for the server to start
@@ -27,10 +30,10 @@ done
 if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; 
 then
     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE ${MYSQL_DATABASE};"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 fi
 
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
