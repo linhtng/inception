a4fdf1139150c866bc5ce87fee1d57a1ccee83a4
linh nguyen <thuynguy@student.hive.fi>
2024-03-28 00:39:17 +0200

    changed wp theme

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -11,7 +11,7 @@ echo "MariaDB is accessible."
 # Create the directory to enable the php-fpm service to start
 mkdir -p /var/www/html /var/www/html/wordpress /run/php/
 chown -R www:www /var/www/html/
-cd /var/www/html;
+cd /var/www/html
 
 if [ ! -f /var/www/html/wp-config.php ]; 
 then
@@ -20,10 +20,9 @@ then
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
     wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
-    wp theme install twentytwentythree --activate --allow-root
+    wp theme install twentytwentytwo --activate --allow-root
 fi
 
-# chown -R www:www /var/www/html/
 
 # Start the PHP-FPM service in the foreground and leave it running constantly with -F
 exec /usr/sbin/php-fpm81 -F;
